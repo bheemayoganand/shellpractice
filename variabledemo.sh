92553a376e1933796a07fa85ca9ddc5ab671b9e5
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-19 22:15:23 +0530

    helloworld script added

--- variabledemo.sh
+++ variabledemo.sh
@@ -2,7 +2,7 @@
 PERSON1=Sachin
 PERSON2=Rohit
 
-echo "$PERSON1: hi suresh, good moring"
+echo "$PERSON1: hi $PERSON2, good moring"
 echo "$PERSON2: Good morning $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: I am good , how you  $PERSON1"
