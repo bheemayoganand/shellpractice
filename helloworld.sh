7b498b48685cae94a132f35f45e08d7743803b54
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-19 22:09:58 +0530

    helloworld script added

--- helloworld.sh
+++ helloworld.sh
@@ -4,3 +4,9 @@ echo "Hello World, I am learning DevOps with AWS"
 echo "i am yoganand"
 echo "This is testing purpose"
 echo "tseting purpose"
+
+
+echo "Ramesh: hi suresh, good moring"
+echo "Suresh: Good morning Ramesh"
+echo "Ramesh: How are you suresh"
+echo "Suresh: I am good , how you  Ramesh"
