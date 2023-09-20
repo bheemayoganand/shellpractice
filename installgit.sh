779454e37b8d10c5f1577a8e33e715f2ef731f2a
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-20 14:23:55 +0530

    code changes

---
+++ installgit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#our program goal is to install mysql
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0] //ne not equal to 
+then
+    echo "Error :: Please run this script with root access"
+#else
+	#echo "Info:: you are root user"
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+	echo "Installation of my sql is error"
+	exit 1
+else
+	echo "Installation of mysql is sucess"
+fi
+
+yum install postfix -y
+
+if [ $? -ne 0]
+then 
+	echo "Installation of postfix is error"
+	exit 1
+else
+	echo "Installation of postfix is success"
+fi
+
+
