b5fb67d6b5cd47e74b40a5ec930c09d926a9833c
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-21 11:22:04 +0530

    code changes

--- createec2.sh
+++ createec2.sh
@@ -1,9 +1,15 @@
 #!bin/bash
 
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user"
- "cart" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0328c338216b5ad4d
+DOMAIN_NAME=joindevops.online
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
 for i in "${NAMES[@]}"
-do
-   
-	echo "NAME: $i"
-done
+do  
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
