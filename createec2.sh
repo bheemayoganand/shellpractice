515a6a4c847a835bd4751b52808bba5c8297acee
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-21 11:09:13 +0530

    code changes

--- createec2.sh
+++ createec2.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user"
+ "cart" "shipping" "payment" "dispatch" "web")
 for in in "${NAMES[@]}"
 do
+    if
 	echo "NAME: $i"
 done
