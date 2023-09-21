5a0f855066cd4deea4626c407b71f30a70a26e50
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-21 11:10:53 +0530

    code changes

--- createec2.sh
+++ createec2.sh
@@ -2,7 +2,7 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user"
  "cart" "shipping" "payment" "dispatch" "web")
-for in in "${NAMES[@]}"
+for i in "${NAMES[@]}"
 do
     if
 	echo "NAME: $i"
